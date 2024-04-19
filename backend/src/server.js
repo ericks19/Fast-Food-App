724c999d87554ca9e771146bd7e85d2100a8e109
ericks19 <ericknz2014@gmail.com>
2024-04-19 17:37:36 -0300

    set to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js'
 import userRouter from './routers/user.router.js'
 import orderRouter from './routers/order.router.js';
-
+import path, { dirname } from 'path';
 
 import { dbconnect } from './config/database.config.js';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -24,7 +28,13 @@ app.use('/api/foods', foodRouter);
 app.use('/api/user', userRouter);
 app.use('/api/orders', orderRouter);
 
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
 
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
