310d11c757ba366e80cdf45d1715478889297ad1
ericks19 <ericknz2014@gmail.com>
2024-04-29 19:57:58 -0300

    changing data to test

--- backend/src/data.js
+++ backend/src/data.js
@@ -1,18 +1,18 @@
 export const sample_foods = [
   {
     id: '1',
-    name: 'Pizzaaaaaaaaaaaaaaaaa Pepperoni',
+    name: 'Pizza Pepperoni',
     cookTime: '10-20',
     price: 10,
     favorite: false,
     origins: ['Italy'],
     stars: 4.5,
     imageUrl: 'food-1.jpg',
-    tags: ['FastFood', 'Pizza', 'Lunch']
+    tags: ['Pizza']
   },
   {
     id: '2',
-    name: 'Meatball',
+    name: 'Meatball The Best',
     cookTime: '20-30',
     price: 20,
     favorite: true,
